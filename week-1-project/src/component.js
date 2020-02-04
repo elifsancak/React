e9fd8b0d3fc0fd0369b5ec85073a17051da4c9a7
elif <elifsancak252@gmail.com>
2020-02-04 14:07:08 +0100

    edited

--- week-1-project/src/component.js
+++ week-1-project/src/component.js
@@ -28,7 +28,7 @@ function NewsArticle() {
 
 function MyApp() {
     const articles = [];
-    for(let i=0; i,200; i++){
+    for(let i=0; i<200; i++){
         articles.push(<NewsArticle />);
     }
 
